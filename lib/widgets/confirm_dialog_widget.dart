fb006f460a44601e2d2c1273dba73cef27a73321
ElectroZombie <daphniphyllum901@gmail.com>
2025-07-15 22:35:52 -0400

    creacion del dialog de confirmacion

---
+++ lib/widgets/confirm_dialog_widget.dart
@@ -0,0 +1,39 @@
+import 'package:educative_software/styles/button_style.dart';
+import 'package:flutter/material.dart';
+
+Future<bool> confirmDialogWidget(String text, BuildContext context) async {
+  ColorScheme colors = Theme.of(context).colorScheme;
+  bool value = true;
+  await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('CONFIRMACION'),
+        backgroundColor: colors.surface,
+        content: Text(
+          text,
+          style: const TextStyle(fontSize: 14),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              value = true;
+              Navigator.pop(context);
+            },
+            style: dialogButtonStyle(Theme.of(context).colorScheme),
+            child: const Text("CONFIRMAR"),
+          ),
+          TextButton(
+            onPressed: () {
+              value = false;
+              Navigator.pop(context);
+            },
+            style: dialogButtonStyle(Theme.of(context).colorScheme),
+            child: const Text("CANCELAR"),
+          ),
+        ],
+      );
+    },
+  );
+  return value;
+}
